3603d41afbe551f881e66fc4382dc91d04d5dcb6
Seeam Islam <seeamislam25@gmail.com>
2017-07-31 21:46:19 -0400

    Add files via upload

---
+++ Java Basics/BadForm.java
@@ -0,0 +1,10 @@
+public class BadForm {
+
+public static void main (String[] args)
+
+{
+
+System.out.println ("What is wrong with this?");
+
+}
+}
---
+++ Java Basics/Diamond.java
@@ -0,0 +1,8 @@
+// Seeam Islam 
+public class Diamond {
+   public static void main (String[] args)
+   {
+      System.out.println ("    *\n   * *\n  * * *\n   * *\n    *");
+   }
+
+}
