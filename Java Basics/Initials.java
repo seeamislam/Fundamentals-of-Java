7d5d1d188da642ccddd2b28da2341e20363892f8
Seeam Islam <seeamislam25@gmail.com>
2017-08-01 22:17:55 -0400

    Add files via upload

---
+++ Java Basics/Initials.java
@@ -0,0 +1,8 @@
+// Seeam Islam 
+public class Initials {
+   public static void main (String[] args)
+   {
+      System.out.println ("     S IIIII\n     S I\n     S IIII\nS    S I\nSSSSSS I");
+   }
+
+}
