324a6dfb1d06b0fd814f67badc8b36987a8fb915
Seeam Islam <seeamislam25@gmail.com>
2017-08-08 21:03:15 -0400

    Add files via upload

---
+++ Math Methods/DemontrateMath.java
@@ -0,0 +1,21 @@
+public class DemontrateMath {
+    public static void main (String[] args)
+{
+System.out.println("Math.abs(-5) = " + Math.abs(-5));
+System.out.println("Math.abs(5) = " + Math.abs(5));
+System.out.println("Math.abs(-10) - Math.abs(6) = " + (Math.abs(-10) - Math.abs(6)));
+System.out.println("Math.pow(4,2) = " + Math.pow(4,2));
+System.out.println("Math.pow(2,4) = " + Math.pow(2,4));
+System.out.println("Math.pow(3,3) = " + Math.pow(3,3));
+System.out.println("Math.sqrt(81) = " + Math.sqrt(81));
+System.out.println("Math.sqrt(0.0064) = " + Math.sqrt(0.0064));
+System.out.println("Math.sqrt(100) = " + Math.sqrt(100));
+System.out.println("Math.round(3.499) = " + Math.round(3.499));
+System.out.println("Math.round(3.501) = " + Math.round(3.501));
+System.out.println("Math.ceil(4.002) = " + Math.ceil(4.002));
+System.out.println("Math.floor(7.99) = " + Math.floor(7.99));
+System.out.println("Math.min(100,2) = " + Math.min(100,2));
+System.out.println("Math.max(100,2) = " + Math.max(100,2));
+System.out.println("Math.PI = " + Math.PI);
+    }
+}
